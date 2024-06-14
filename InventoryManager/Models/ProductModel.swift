4b2c4fc05e4d0e5b227f44ec62ec803abf473cab
Marcelo de Abreu <marcelodeabreu@Marcelos-MacBook-Pro.local>
2024-06-14 15:19:31 -0300

    datamodelcommit

--- InventoryManager/Models/ProductModel.swift
+++ InventoryManager/Models/ProductModel.swift
@@ -7,8 +7,28 @@
 
 import Foundation
 
+import Foundation
+
+struct Category: Identifiable {
+    let id = UUID()
+    var name: String
+    var products: [Product]
+}
+
+struct Product: Identifiable {
+    let id = UUID()
+    var name: String
+    var quantity: Int
+    var productPrice = 0
+}
+
+
+
+/*
 struct Product {
     static var ID = UUID()
     static var productName: String = ""
     static var productPrice: Int = 0
+    static var category: [String] = [""]
 }
+*/
